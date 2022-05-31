e4181596ff3557662c72e1334db53415bfa91b6c
vitorbrambilla <vitorbrambilla10@gmail.com>
2022-05-31 16:36:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Hooks
+import { useAuth } from './hooks/useAuth';
+
+// router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// pages
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import EditProfile from './pages/EditProfile/EditProfile';
+import Profile from './pages/Profile/Profile';
+import Photo from './pages/Photo/Photo';
+import Search from './pages/Search/Search';
+
 function App() {
+  const { auth, loading } = useAuth();
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={auth ? <Home /> : <Navigate to='/login' />}
+            />
+
+            <Route
+              path='/profile'
+              element={auth ? <EditProfile /> : <Navigate to='/login' />}
+            />
+
+            <Route
+              path='/users/:id'
+              element={auth ? <Profile /> : <Navigate to='/login' />}
+            />
+
+            <Route
+              path='/search'
+              element={auth ? <Search /> : <Navigate to='/login' />}
+            />
+
+            <Route
+              path='login'
+              element={!auth ? <Login /> : <Navigate to='/' />}
+            />
+
+            <Route
+              path='register'
+              element={!auth ? <Register /> : <Navigate to='/' />}
+            />
+            
+            <Route path='photos/:id' element={<Photo />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
